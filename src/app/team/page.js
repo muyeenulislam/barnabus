0505a0df7f606dd39db927764cca5964ae2074ca
muyeen <muyeenulislamsakif@gmail.com>
2025-10-21 20:40:46 +0600

    add suspense

--- src/app/team/page.js
+++ src/app/team/page.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 
 import TeamAndPartnership from "@/components/team/team-and-partnerships";
 import JoinRnd from "@/components/home/join-rnd";
 
 const Page = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <TeamAndPartnership />
       <JoinRnd />
-    </>
+    </Suspense>
   );
 };
 
